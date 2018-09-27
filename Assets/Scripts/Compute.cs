88ef32fcdc23fc8e46238dad4ffe9e9aa291a1ca
win_yasohasegawa <dev@yasuohasegawa.com>
2018-09-27 17:02:50 +0900

    added compute shader example.

---
+++ Assets/Scripts/Compute.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Compute : MonoBehaviour {
+    const int BLOCK_SIZE = 256;
+
+    public Mesh mesh;
+    public Material material;
+    public ComputeShader computeShader;
+
+    private Bounds bounds;
+    private ComputeBuffer positionBuffer;
+    private ComputeBuffer visibleBuffer;
+    private ComputeBuffer animationBuffer;
+    private ComputeBuffer argsBuffer;
+    private uint[] args = new uint[5];
+    private uint numeberOfCubes = 10000;
+
+    private Vector3[] positions;
+    private int[] visible;
+    private Vector3 maxPos = new Vector3(5f, 5f, 3f);
+
+    // Use this for initialization
+    void Start () {
+        // 12 = float (4 byte x 3）
+        positionBuffer = new ComputeBuffer((int)numeberOfCubes, 12);
+        visibleBuffer = new ComputeBuffer((int)numeberOfCubes, sizeof(int));
+        animationBuffer = new ComputeBuffer((int)numeberOfCubes, sizeof(float));
+        positions = new Vector3[numeberOfCubes];
+
+        visible = new int[numeberOfCubes];
+        float[] animationValue = new float[numeberOfCubes];
+        for (int i = 0; i < numeberOfCubes; i++)
+        {
+            float dist = 50f;
+            float deg = numeberOfCubes / 360f;
+            float rad = (deg * (float)i) * Mathf.Deg2Rad;
+            float x = Mathf.Cos(rad) * dist;
+            float z = Mathf.Sin(rad) * dist;
+            float scaleVal = Random.Range(-0.1f, 0.3f);
+            Vector3 pos = new Vector3(x + Random.Range(-maxPos.x, maxPos.x), Random.Range(-maxPos.y, maxPos.y), z + Random.Range(-maxPos.x, maxPos.y));
+            positions[i] = pos;
+            animationValue[i] = 0f;
+            visible[i] = 1;
+        }
+
+        positionBuffer.SetData(positions);
+        visibleBuffer.SetData(visible);
+        animationBuffer.SetData(animationValue);
+        material.SetBuffer("PositionBuffer", positionBuffer);
+        material.SetBuffer("VisibleBuffer", visibleBuffer);
+        material.SetBuffer("AnimationBuffer", animationBuffer);
+
+        bounds = new Bounds(Vector3.zero, new Vector3(numeberOfCubes / 3, numeberOfCubes / 3, numeberOfCubes / 3));
+
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        args[0] = mesh.GetIndexCount(0);
+        args[1] = numeberOfCubes;
+        args[2] = mesh.GetIndexStart(0);
+        args[3] = mesh.GetBaseVertex(0);
+        args[4] = 0;
+        argsBuffer.SetData(args);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i< visible.Length; i++)
+        {
+            if (i< visible.Length/2)
+            {
+                visible[i] = 1;
+            } else
+            {
+                visible[i] = 0;
+            }
+        }
+
+        visibleBuffer.SetData(visible);
+        material.SetBuffer("VisibleBuffer", visibleBuffer);
+
+        int kernelId = computeShader.FindKernel("CSMain");
+        computeShader.SetBuffer(kernelId, "PositionBuffer", positionBuffer);
+        computeShader.SetBuffer(kernelId, "VisibleBuffer", visibleBuffer);
+        computeShader.SetBuffer(kernelId, "AnimationBuffer", animationBuffer);
+        int groupSize = Mathf.CeilToInt(numeberOfCubes / BLOCK_SIZE);
+        computeShader.Dispatch(kernelId, groupSize, 1, 1);
+        computeShader.SetFloat("_Time", Time.time);
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer);
+    }
+
+    void OnDestroy()
+    {
+        visibleBuffer.Release();
+        animationBuffer.Release();
+        positionBuffer.Release();
+        argsBuffer.Release();
+    }
+}
---
+++ Assets/Scripts/Compute.shader
@@ -0,0 +1,75 @@
+﻿Shader "Custom/Compute" {
+	Properties{
+		_Color("Color", Color) = (1,1,1,1)
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+	}
+
+	SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 200
+
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows vertex:vert
+		#pragma multi_compile_instancing
+		#pragma instancing_options procedural:setup
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			StructuredBuffer<float3> PositionBuffer;
+			StructuredBuffer<float> AnimationBuffer;
+			StructuredBuffer<int> VisibleBuffer;
+		#endif
+
+		void setup() {
+			#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+				float3 verts = PositionBuffer[unity_InstanceID];
+				float x = verts.x;
+				float y = verts.y;
+				float z = verts.z;
+				unity_ObjectToWorld._14_24_34_44 = float4(x, y, z, 1);
+			#endif
+		}
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+
+		void vert(inout appdata_full v) {
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			float animationVal = AnimationBuffer[unity_InstanceID];
+			float3 scale = float3(abs(animationVal), abs(animationVal), abs(animationVal));
+
+			float4x4 scaleMat = float4x4(
+				scale.x, 0, 0, 0,
+				0, scale.y, 0, 0,
+				0, 0, scale.z, 0,
+				0, 0, 0, 1
+			);
+
+			v.vertex.y += animationVal;
+			v.vertex.xyz = mul(v.vertex.xyz, scaleMat);
+#endif
+		}
+
+		void surf(Input IN, inout SurfaceOutputStandard o) {
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			if (VisibleBuffer[unity_InstanceID] == 0) {
+				o.Albedo = float4(1.0,0.0,0.0,1.0);
+			}
+			else
+			{
+				o.Albedo = _Color;
+			}
+#endif
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = 1;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
